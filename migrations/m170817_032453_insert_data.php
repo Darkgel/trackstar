7009cbab0f35794301bf42f4bd09458788b09064
Darkgel <1627116986@qq.com>
2017-10-16 21:58:57 +0800

    1.bug fix: 数据修改

--- migrations/m170817_032453_insert_data.php
+++ migrations/m170817_032453_insert_data.php
@@ -12,16 +12,6 @@ class m170817_032453_insert_data extends Migration
         $test = User::find()->where('username=:username', [':username'=>'test'])->one();
 
 
-        //将test用户与project 1 关联
-        $this->insert('{{%project_user_assignment}}',[
-            'project_id' => '1',
-            'user_id' => $test->id,
-            'create_time' => '2017-08-08 13:33:33',
-            'create_user_id' => '1',
-            'update_time' => '2017-08-08 13:33:33',
-            'update_user_id' => '1',
-        ]);
-
         //往project_user_role中添加数据
         $this->insert('{{%project_user_role}}',[
             'project_id' => '1',
@@ -69,11 +59,6 @@ class m170817_032453_insert_data extends Migration
     public function safeDown()
     {
         $this->truncateTable('{{%project_user_role}');
-
-        $test = User::find()->where('username=:username', [':username'=>'test'])->one();
-        $this->delete('{{%project_user_assignmnet}','project_id=1 and user_id=:user_id', [
-            ':user_id' => $test->id,
-        ]);
     }
 
     /*
